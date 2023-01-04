a65c9bff3f4eca54baefe4b948635be687042e7d
Emre SAVAŞ <45127347+emre-savas@users.noreply.github.com>
2023-01-04 08:14:32 +0300

    added unit test event bus

--- tests/BuildingBlocks/EventBus.XUnitTest/EventBusTests.cs
+++ tests/BuildingBlocks/EventBus.XUnitTest/EventBusTests.cs
@@ -1,5 +1,11 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using EventBus.Base;
+using EventBus.Base.Abstraction;
+using EventBus.Factory;
+using EventBus.XUnitTest.Events.EventHandlers;
+using EventBus.XUnitTest.Events.Events;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
 
 namespace EventBus.XUnitTest
 {
@@ -14,9 +20,35 @@ namespace EventBus.XUnitTest
         }
 
 
-        [TestMethod]
+        
         public void subscribe_event_on_rabbitmq_test()
         {
+            services.AddSingleton<IEventBus>(e =>
+            {
+                EventBusConfig config = new()
+                {
+                    ConnectionRetryCount = 5,
+                    SubscriberClientAppName = "EventBus.UnitTest",
+                    DefaultTopicName = "SvsBusAppointmentTopicName",
+                    EventBusType = EventBusType.RabbitMQ,
+                    EventNamePrefix = "IntegrationEvent",
+                    //Connection = new ConnectionFactory()
+                    //{
+                    //    HostName = "localhost",
+                    //    Port = 5672,
+                    //    UserName = "guest",
+                    //    Password = "guest"
+                    //};
+                };
+                return EventBusFactory.Create(config, e);
+            });
+
+            var sp = services.BuildServiceProvider();
+
+            var eventbus = sp.GetRequiredService<IEventBus>();
+
+            eventbus.Subscribe<OrderCreatedIntegrationEvent,OrderCreatedIntegrationEventHandler>();
+
 
         }
     }
